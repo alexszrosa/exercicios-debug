98d94ac36b260b18883eab73a73e3545baba8598
Alexandre Rosa <prcriandoseubot@gmail.com>
2025-08-20 15:14:22 -0300

    Exercícios de 1 a 3 resolvidos

--- exercicio.py
+++ exercicio.py
@@ -1,19 +1,46 @@
-### Exercício 1: Verificação de Qualidade de Dados
-# Você está analisando um conjunto de dados de vendas e precisa garantir 
-# que todos os registros tenham valores positivos para `quantidade` e `preço`. 
-# Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
-# forem positivos ou "Dados inválidos" caso contrário.
 
-### Exercício 2: Classificação de Dados de Sensor
-# Imagine que você está trabalhando com dados de sensores IoT. 
-# Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
-# como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# ### Exercício 1: Verificação de Qualidade de Dados
+# # Você está analisando um conjunto de dados de vendas e precisa garantir 
+# # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
+# # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
+# # forem positivos ou "Dados inválidos" caso contrário.
+# quantidade = 10
+# preco = 5
+# if quantidade > 0 and preco > 0:
+#     print('Dados válidos')
+# else:
+#     print('Dados inválidos')
+
+
+# ### Exercício 2: Classificação de Dados de Sensor
+# # Imagine que você está trabalhando com dados de sensores IoT. 
+# # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
+# # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# temperatura = 39
+# if temperatura < 30:
+#     print('Temperatura baixa')
+# elif temperatura == 30:
+#     print('Temperatura Normal')
+# else:
+#     print('Temperatura Alta')
+
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
 # com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+
+# log = {
+#     'timestamp': '2021-06-23 10:00:00',
+#     'level': 'ERROR',
+#     'message': 'Falha na conexão'
+# }
+
+# # Verifique se a severidade é 'ERROR'
+# if log['level'] == 'ERROR':
+#     # Imprima a mensagem
+#     print(log['message'])
 
 ### Exercício 4: Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
